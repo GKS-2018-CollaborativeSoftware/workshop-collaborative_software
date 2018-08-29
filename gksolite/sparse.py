697e7dd941d0107b495a3884410be49ef931c46b
dseith <davidseith@web.de>
2018-08-29 15:20:45 +0200

    fixed gksolite misspelling

--- gksolite/sparse.py
+++ gksolite/sparse.py
@@ -3,7 +3,7 @@ class SetGol(object):
     Game of Life implemented via sets
 
     :param board: the initial state of the board
-    :type board: :py:class:`gksol.boards.PaddedBoard` or List[List[int]]
+    :type board: :py:class:`gksolite.boards.PaddedBoard` or List[List[int]]
 
     .. describe:: gol[n]
 
